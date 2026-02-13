7a1e5e860562b423de2568be6ddcc7b97ddced79
Ebin Louis <ebinlouis1559@gmail.com>
2026-02-13 23:18:30 +0530

    basic routing completed

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,10 @@ import { ENV } from './lib/env.js';
 const app = express();
 const PORT = ENV.PORT;
 
+app.get('/', (req, res) => {
+    res.status(200).send('Hello');
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on the port : ${PORT}`);
 });
