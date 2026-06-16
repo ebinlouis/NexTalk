6811f6019335a28eb859f15a25c4ce6b08d945c8
Ebin Louis <ebinlouis1559@gmail.com>
2026-06-16 15:39:58 +0530

    feat: setup message model, auth middleware, and contacts API

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.route.js';
+import messageRoutes from './routes/message.route.js';
 
 const app = express();
 
@@ -8,5 +9,6 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/messages', messageRoutes);
 
 export default app;
